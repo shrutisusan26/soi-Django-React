ed1766c1636009444aec8680156a5aa4eec7e7e6
shrutisusan26 <shrutisusan055@gmail.com>
2021-03-08 23:02:31 +0530

    Backend profile

--- backend/sapp/models.py
+++ backend/sapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from login.models import Startup
 # Create your models here.
+class Profile(models.Model):
+    profile_user=models.OneToOneField(Startup, on_delete=models.CASCADE,primary_key=True)
+    place=models.CharField(max_length=100)
+    tags=models.CharField(max_length=100)
+    logo_url=models.CharField(max_length=100)
+
+
--- backend/soi/urls.py
+++ backend/soi/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('login.urls')),
     url('auth/', obtain_auth_token),
+    url('', include('sapp.urls')),
 ]
