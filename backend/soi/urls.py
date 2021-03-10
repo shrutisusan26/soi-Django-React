b278a706e170ef39de10ba4d9186e2f14372a842
shrutisusan26 <shrutisusan055@gmail.com>
2021-03-10 17:25:51 +0530

    Token customization

--- backend/soi/urls.py
+++ backend/soi/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from rest_framework.authtoken.views import obtain_auth_token
-
+from login.serializers import CustomAuthToken
 """ obtain_auth_token is responsible for giving tokens with regard to each user """
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('login.urls')),
-    url('auth/', obtain_auth_token),
+    path('api-token-auth/', CustomAuthToken.as_view()),
     url('', include('sapp.urls')),
 ]
