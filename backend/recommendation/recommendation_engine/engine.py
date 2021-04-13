b8b22b6b128d5247a54fd528e3605c07c8ccdffd
JinitSan <jinitsanghvi@gmail.com>
2021-04-13 17:43:51 +0530

    switched recommendation engine to class-based approach

--- backend/recommendation/recommendation_engine/engine.py
+++ backend/recommendation/recommendation_engine/engine.py
@@ -30,86 +30,91 @@ corpus_tfidf_vectorizer = joblib.load("corpus_tfidf_vectorizer.pkl")
 corpus_vocabulary = defaultdict(None, copy.deepcopy(corpus_tfidf_vectorizer.vocabulary_))
 corpus_vocabulary.default_factory = corpus_vocabulary.__len__
 cosine_similarities = joblib.load("cosine_similarities.pkl")
-
-def lemmatize(text):
-    return WordNetLemmatizer().lemmatize(text, pos='v')
-
-
-def preprocess(raw_text):
-
-    # keep only words
-    letters_only_text = re.sub("[^a-zA-Z]", " ", raw_text)
-
-    # convert to lower case and split 
-    words = letters_only_text.lower()
-
-    return [lemmatize(token) for token in gensim.utils.simple_preprocess(words) if (token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3) ]
-
-def loadGloveModel(gloveFile):
-    print ("Loading Glove Model")
-    with open(gloveFile, encoding="utf8" ) as f:
-        content = f.readlines()
-    model = {}
-    for line in content:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print ("Done.",len(model)," words loaded!")
-    return model
-
-def add_new_embeddings(queries, embeddings, tfidf, vocab, cosine_similarities):
-
-  pre_query = [preprocess(query) for query in queries]
-
-  query_vocabulary_vectorizer = TfidfVectorizer()
-  query_vocabulary_vectorizer.fit_transform([' '.join(query) for query in pre_query])
-  for word in query_vocabulary_vectorizer.vocabulary_.keys():
-    corpus_vocabulary[word]
-
-  query_tfidf = TfidfVectorizer(vocabulary=corpus_vocabulary).fit_transform([' '.join(query) for query in pre_query])
-
-  tfidf.resize((tfidf.shape[0],query_tfidf.shape[1]))
-  tfidf = sp.vstack([tfidf, query_tfidf])
-
-  tfidf = tfidf.toarray()  
-  
-  for query in pre_query:
-    
-    embedding = np.zeros(50)
-    tf_wts = 0
-    dic_wts = {}
-    features = dict(corpus_vocabulary)
-    
-    for w in query:
-      embedding += tfidf[-1][features[w]]*model.get(w,0)
-      tf_wts += tfidf[-1][features[w]]
-
-    cosine_sim = cosine_similarity(embedding.reshape(1,-1),embeddings)
-
-    cosine_similarities = np.vstack((cosine_similarities,np.ones((1,cosine_similarities.shape[1]))))
-    cosine_similarities = np.hstack((cosine_similarities,np.ones((cosine_similarities.shape[0],1))))
-
-    cosine_similarities[-1,:-1] = cosine_sim
-    cosine_similarities[:-1,-1] = cosine_sim
-    cosine_similarities[-1,-1] = 1
-
-    embeddings.append(embedding/tf_wts)
-
-  return embeddings,sp.csr_matrix(tfidf),corpus_vocabulary, cosine_similarities
-
-def Recommend(name,embeddings,cosine_similarities):
-  idx = df[df['Name']==name].index[0]
-  sim_scores = list(enumerate(cosine_similarities[idx]))
-  sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
-  sim_scores = sim_scores[1:8]
-  startup_indices = [i[0] for i in sim_scores]
-  recommend = df.iloc[startup_indices]
-  print(recommend)
   
 df = pd.read_excel("P11-1000-Startups.xlsx",engine='openpyxl')
 df.drop([334,982],inplace=True)
 
-model = loadGloveModel(gloveFile)
+def lemmatize(text):
+    return WordNetLemmatizer().lemmatize(text, pos='v')
 
-Recommend('Cankix',embeddings,cosine_similarities)
+class Engine:
+    def __init__(self,embeddings=embeddings,tfidf=tfidf,corpus_vocabulary=corpus_vocabulary,cosine_similarities=cosine_similarities,df=df):
+        self.embeddings = embeddings
+        self.tfidf = tfidf
+        self.corpus_vocabulary = corpus_vocabulary
+        self.cosine_similarities = cosine_similarities
+        self.df = df
+    
+    def preprocess(self, raw_text):
+        # keep only words
+        letters_only_text = re.sub("[^a-zA-Z]", " ", raw_text)
+        # convert to lower case and split 
+        words = letters_only_text.lower()
+        return [lemmatize(token) for token in gensim.utils.simple_preprocess(words) if (token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3) ]
+    
+    def loadGloveModel(self,gloveFile="/home/jinitsan/Documents/glove.6B.50d.txt"):
+        print ("Loading Glove Model")
+        with open(gloveFile, encoding="utf8" ) as f:
+            content = f.readlines()
+        model = {}
+        for line in content:
+            splitLine = line.split()
+            word = splitLine[0]
+            embedding = np.array([float(val) for val in splitLine[1:]])
+            model[word] = embedding
+        print ("Done.",len(model)," words loaded!")
+        self.model = model
+    
+    def add_new_embeddings(self,queries):
+
+        pre_query = [self.preprocess(query) for query in queries]
+
+        query_vocabulary_vectorizer = TfidfVectorizer()
+        query_vocabulary_vectorizer.fit_transform([' '.join(query) for query in pre_query])
+        for word in query_vocabulary_vectorizer.vocabulary_.keys():
+            corpus_vocabulary[word]
+
+        query_tfidf = TfidfVectorizer(vocabulary=self.corpus_vocabulary).fit_transform([' '.join(query) for query in pre_query])
+
+        tfidf.resize((self.tfidf.shape[0],query_tfidf.shape[1]))
+        tfidf = sp.vstack([self.tfidf, query_tfidf])
+
+        self.tfidf = self.tfidf.toarray()  
+        
+        for query in pre_query:
+            
+            embedding = np.zeros(50)
+            tf_wts = 0
+            dic_wts = {}
+            features = dict(self.corpus_vocabulary)
+            
+            for w in query:
+                embedding += self.tfidf[-1][features[w]]*self.model.get(w,0)
+                tf_wts += self.tfidf[-1][features[w]]
+
+            cosine_sim = cosine_similarity(embedding.reshape(1,-1),self.embeddings)
+
+            self.cosine_similarities = np.vstack((self.cosine_similarities,np.ones((1,self.cosine_similarities.shape[1]))))
+            self.cosine_similarities = np.hstack((self.cosine_similarities,np.ones((self.cosine_similarities.shape[0],1))))
+
+            cosine_similarities[-1,:-1] = cosine_sim
+            cosine_similarities[:-1,-1] = cosine_sim
+            cosine_similarities[-1,-1] = 1
+
+            self.embeddings.append(embedding/tf_wts)
+
+        self.tfidf = sp.csr_matrix(self.tfidf)
+    
+    
+    def Recommend(self,name):
+        idx = df[df['Name']==name].index[0]
+        sim_scores = list(enumerate(self.cosine_similarities[idx]))
+        sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+        sim_scores = sim_scores[1:8]
+        startup_indices = [i[0] for i in sim_scores]
+        recommend = df.iloc[startup_indices]
+        print(recommend)
+
+engine = Engine()
+engine.loadGloveModel()
+engine.Recommend("Daltfase")
