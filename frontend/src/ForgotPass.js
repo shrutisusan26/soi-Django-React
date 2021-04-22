deec1f7cf71a45c4d1f50ccc4a6c448286d03d40
abhishekrai2304 <abhishekrai2304@gmail.com>
2021-04-22 14:53:12 +0530

    styled forgot pass

--- frontend/src/ForgotPass.js
+++ frontend/src/ForgotPass.js
@@ -1,7 +1,35 @@
 import React,{useState} from 'react'
 import APIService from "./APIService"
 import {useHistory} from 'react-router-dom'
+import {
+    Button,
+    NavItem,
+    NavLink,
+    Nav,
+    TabContent,
+    TabPane,
+    Container,
+    Row,
+    Col,
+    UncontrolledTooltip,
+  } from "reactstrap";
+import ExamplesNavbar from "./components/Navbars/ExamplesNavbar";
+import DefaultFooter from "./components/Footers/DefaultFooter";
+import DarkFooter from "./components/Footers/DarkFooter";
+import ForgotHeader from "./components/Headers/ForgotHeader";
 function ForgotPass() {
+    const [pills, setPills] = React.useState("2");
+    React.useEffect(() => {
+      document.body.classList.add("profile-page");
+      document.body.classList.add("sidebar-collapse");
+      document.documentElement.classList.remove("nav-open");
+      window.scrollTo(0, 0);
+      document.body.scrollTop = 0;
+      return function cleanup() {
+        document.body.classList.remove("profile-page");
+        document.body.classList.remove("sidebar-collapse");
+      };
+    }, []);
     let history=useHistory();
     const [email,setEmail]=useState('');
     const handleSubmit=()=>{
@@ -14,6 +42,109 @@ function ForgotPass() {
     }
     
     return (
+        <>
+        <ExamplesNavbar />
+        <div className="wrapper">
+        <ForgotHeader />
+        <div className="section">
+          <Container>
+            <div className="button-container">
+              
+            </div>
+            
+            <Row>
+              <Col className="ml-auto mr-auto" md="6">
+                {/* <h4 className="title text-center">My Portfolio</h4> */}
+                <div className="nav-align-center">
+                  <Nav
+                    className="nav-pills-info nav-pills-just-icons"
+                    pills
+                    role="tablist"
+                  >
+                    <NavItem>
+                     
+                    </NavItem>
+                    <NavItem>
+                      
+                    </NavItem>
+                    <NavItem>
+                    
+                    </NavItem>
+                  </Nav>
+                </div>
+              </Col>
+              <TabContent className="gallery" activeTab={"pills" + pills}>
+                <TabPane tabId="pills1">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                         
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                         
+                        ></img>
+                      </Col>
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                          
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                         
+                        ></img>
+                      </Col>
+                    </Row>
+                  </Col>
+                </TabPane>
+                <TabPane tabId="pills2">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                     
+                     
+                    </Row>
+                  </Col>
+                </TabPane>
+                <TabPane tabId="pills3">
+                  <Col className="ml-auto mr-auto" md="10">
+                    <Row className="collections">
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                        //   src={bg3}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                        //   src={bg8}
+                        ></img>
+                      </Col>
+                      <Col md="6">
+                        <img
+                          alt="..."
+                          className="img-raised"
+                        //   src={bg7}
+                        ></img>
+                        <img
+                          alt="..."
+                          className="img-raised"
+                        //   src={bg6}
+                        ></img>
+                      </Col>
+                    </Row>
+                  </Col>
+                </TabPane>
+              </TabContent>
+            </Row>
+          </Container>
         <div>
             <div className="login" style={centerElem}>
                 <br/>
@@ -32,6 +163,10 @@ function ForgotPass() {
                 </div>
         </div>
         </div>
+        </div>
+        <DarkFooter />
+        </div>
+        </>
     )
 }
 
